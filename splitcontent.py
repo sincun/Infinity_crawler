5e4018cb6336c763bcb90ab75376f75846e38851
sincun song <930428924@qq.com>
2018-10-09 07:26:08 +0800

    add automatic page analyses

--- splitcontent.py
+++ splitcontent.py
@@ -2,6 +2,7 @@ import re
 from setting import *
 import traceback
 import json
+from queue import Queue
 
 
 class splitContent():
@@ -19,13 +20,16 @@ class splitContent():
 		#存放标签列表，存放正在处理的标签，处理完成后从列表清除
 		self.s_LableList = []
 		self.htmlDict = {}
+		#drop
 		self.startParttern = re.compile(r'^\s*<(\w+)|>\s*<(\w+)')
 		self.endParttern = re.compile(r'\s*(/\w*)>\s*$|\s*(/\w*)>\s*<')
+
 		self.spaceParttern = re.compile(r'\s')
-		self.endMarkParttern = re.compile(r'<?(/\w*)>')
+		self.endMarkParttern = re.compile(r'</(\w*)\s*>')
 		# for fetcheLables
 		self.s_LableParttern = re.compile(r'<(\w+)')
-		self.standardEndLableP = re.compile(r'/(\w*)>')
+		#drop
+		self.standardEndLableP = re.compile(r'/(\w*)\s*>')
 		self.t_startLable = None
 		self.t_endLable = None
 		self.non_endLable = NON_ENDLABLE
@@ -33,7 +37,8 @@ class splitContent():
 		#判处处理的属性是注释或其他非标准内容
 		self.extraAttrSign = False
 		#注释
-		self.annotation_lable = None
+		self.annotation_lable = 0
+		self.annotation_start = False
 		#存放非标准内容
 		self.extraAttr = ""
 		# 统计处理标签个数
@@ -42,7 +47,14 @@ class splitContent():
 		self.lableAttrRecord = 0
 		# 重复标签重命名
 		self.globalKey = None
-	
+
+		#标签处理位置记录
+		self.scriptlablestatus = "readly"
+		#标签属性外的内容
+		self.persingleAttribute = ''
+		#结束标签获取
+		self.nonspace = True
+
 	def fetcheLables(self, line):
 		"""
 
@@ -58,18 +70,36 @@ class splitContent():
 			if atb == '<':
 				continue
 			elif atb == '<!--':
-				self.annotation_lable = True
-			elif atb == '--' and self.annotation_lable:
-				self.annotation_end = True
-			elif atb == '>' or atb == '/>':
+				self.annotation_lable += 1
+				self.extraAttr += atb
+				continue
+			elif atb == '-->' and self.annotation_lable:
+				self.annotation_lable -= 1
+				self.extraAttr += atb
+				if not self.annotation_lable:
+					t_attData = self.mapDict(self.extraAttr, keys="annotation")
+					self.gen_ExpectionDict(t_attData)
+					self.extraAttr = ""
+				continue
+			elif self.annotation_lable:
+				self.extraAttr += atb
+				continue
+			elif atb == '>' or re.match(r'/\s*>',atb):
 				
 				try:
 					# 处理没有结束标签的标签，在setting中设置
 					if self.s_LableList and self.t_startLable.group(1) in self.non_endLable:
 						self.s_LableList.pop()
+
 					self.t_startLable = None
+
 				except:
-					if self.extraAttrSign or (self.annotation_lable and self.annotation_end):
+					LOGGER.error("delete Lable failed {0}".format(self.t_startLable))
+					recodeExcept(*sys.exc_info())
+					LOGGER.error(traceback.format_exc())
+					LOGGER.error("Except EOF")
+					#存在不标准内容或注释时处理，
+					if self.extraAttrSign:
 						LOGGER.error("write Lable extraAttr {0}".format(self.globalKey))
 						t_attData = self.mapDict(self.extraAttr + atb, keys="extraAttr")
 						self.gen_ExpectionDict(t_attData)
@@ -77,16 +107,11 @@ class splitContent():
 						self.extraAttrSign = False
 						self.extraAttr = ""
 						self.annotation_lable = False
-						self.annotation_end = False
 						continue
 					elif self.annotation_lable:
 						self.extraAttr += atb
 						continue
 
-					LOGGER.error("delete Lable failed {0}".format(self.t_startLable))
-					recodeExcept(*sys.exc_info())
-					LOGGER.error(traceback.format_exc())
-					LOGGER.error("Except EOF")
 					exit(100)
 				continue
 			# 存在起始标签,处理标签中属性
@@ -122,19 +147,22 @@ class splitContent():
 					self.lableAttrRecord = 0
 					self.globalKey = None
 				else:
-					self.t_endLable = self.standardEndLableP.match(atb)
+					self.t_endLable = self.endMarkParttern.match(atb)
 					# 如果当前值带有结束标签
 					if self.t_endLable:
 						#
 						# 标签是否结束
 						endSymbol = self.t_endLable.group(1)
 						if endSymbol:
-							LOGGER.debug("clear end lable {0},LableList is {1}".format(endSymbol, self.s_LableList))
+							LOGGER.info("clear end lable {0},LableList is {1}".format(endSymbol, self.s_LableList))
 							try:
-								while len(self.s_LableList) > 0:
-									l_endSymbol = self.s_LableList.pop()
-									if l_endSymbol == endSymbol:
-										break
+								if endSymbol in self.s_LableList:
+									while len(self.s_LableList) > 0:
+										l_endSymbol = self.s_LableList.pop()
+										if l_endSymbol == endSymbol:
+											break
+								else:
+									LOGGER.warn("end tag is alone {0}".format(endSymbol))
 								# 标签结束存在注释，重置标签属性计数
 								self.lableAttrRecord = 0
 								self.globalKey = None
@@ -149,17 +177,9 @@ class splitContent():
 							self.lableAttrRecord = 0
 							self.globalKey = None
 					else:
-						if not self.s_LableList:
-							LOGGER.warn("start lableList is null,attribute formal error {0}".format(atb))
-							continue
-						elif self.extraAttrSign:
-							self.extraAttr += atb
-							continue
-						# 处理额外的非标准标签，如注释<!-->
-						elif re.match(r'<', atb):
-							self.extraAttrSign = True
-							self.extraAttr += repr(atb)
-							continue
+
+						#self.extraAttrSign = True
+						#self.extraAttr += repr(atb)
 						
 						#    self.s_LableList.pop()
 						t_attData = self.mapDict(atb, keys="values")
@@ -278,7 +298,10 @@ class splitContent():
 			kv_data = t_htmlDict
 			desc_k.pop()
 		LOGGER.debug("kv_data is {0}".format(kv_data))
-		self.htmlDict = kv_data
+		if isinstance(kv_data,list):
+			self.htmlDict = {kv_data[0]:kv_data[1]}
+		elif isinstance(kv_data,dict):
+			self.htmlDict = kv_data
 	
 	def standardContent(self, startLable, endLable, line):
 		
@@ -304,13 +327,44 @@ class splitContent():
 			# LOGGER.debug("split char is {0} ,stopCode {1}".format(char,self.stopCode))
 			# stopCode存放双引号个数，每出现两个双引号后重置stopCode，如果存在stopCode，说明关键属性在双引号中，则忽略
 			# scopeLable等于">"说明一个标签的结束，可以返还获取到的内容了
-			if self.spaceParttern.match(char) and not self.stopCode and self.scopeLable != ">":
+			if  not self.stopCode and self.singleAttribute == '<!--':
+				self.annotation_start = True
+				if self.persingleAttribute.strip():
+					diff = yield self.persingleAttribute
+					self.persingleAttribute = ""
+				diff = yield self.singleAttribute
+				self.singleAttribute = ""
+				self.singleAttribute += char
+				continue
+			elif self.annotation_start:
+				self.singleAttribute += char
+				annotation_content = self.singleAttribute[:-3]
+				annotation_end = self.singleAttribute[-3:]
+				if annotation_end == '-->':
+					self.annotation_start = False
+					diff = yield annotation_content
+					annotation_content = ""
+					diff = yield annotation_end
+					annotation_end = ""
+				continue
+
+			if self.spaceParttern.match(char) and not self.stopCode and self.scopeLable != ">" and self.nonspace:
 				# singleAttribute为获取的属性，为空则忽略
 				if not self.singleAttribute:
 					continue
-				
-				# LOGGER.debug("return attribute content is {0}.".format(self.singleAttribute))
-				diff = yield self.singleAttribute
+				elif self.s_LableParttern.match(self.singleAttribute) and (self.scriptlablestatus == "readly" or self.scopeLable == '<'):
+					self.scriptlablestatus = "mark"
+					LOGGER.debug("return attribute content is {0}.".format(self.singleAttribute))
+					if self.persingleAttribute.strip():
+						diff = yield self.persingleAttribute
+						self.persingleAttribute = ""
+					diff = yield self.singleAttribute
+				#处理标签中属性，如：<div class="mask-dark">中的class="mask-dark"
+				elif self.scriptlablestatus == "mark":
+					diff = yield self.singleAttribute
+				else:
+					self.persingleAttribute += self.singleAttribute
+					self.scopeLable = ">"
 				self.singleAttribute = ''
 			else:
 				#处理""中的内容不做拆分，stopCode为封闭符
@@ -319,8 +373,12 @@ class splitContent():
 						self.stopCode.append(char)
 					elif char in self.stopCode:
 						self.stopCode.pop(-1)
+						self.singleAttribute += char
+						diff = yield self.singleAttribute
+						self.singleAttribute = ""
+						continue
 
-				elif char == "<":
+				elif char == "<" and not self.stopCode:
 					# 处理特殊标签，如<em>
 					if char == self.scopeLable:
 						LOGGER.warn("line data style error!present line {0}.".format(line))
@@ -329,45 +387,66 @@ class splitContent():
 						# 打上标签状态"<",标签新的开始，返还之前获取属性
 						self.scopeLable = char
 						# 去除左右两端空格再判断
-						self.singleAttribute = self.singleAttribute.strip()
-						if self.singleAttribute and not self.spaceParttern.match(self.singleAttribute):
-							LOGGER.debug("return start lable attribute is {0}.".format(self.singleAttribute))
-							diff = yield self.singleAttribute
-							self.singleAttribute = ''
-						else:
-							LOGGER.info("LF and  attribute null will be ignore")
-							self.singleAttribute = ''
+						#self.singleAttribute = self.singleAttribute.strip()
+						self.persingleAttribute += self.singleAttribute
+						#if self.singleAttribute and not self.spaceParttern.match(self.singleAttribute):
+						#	LOGGER.debug("return start lable attribute is {0}.".format(self.singleAttribute))
+						#	diff = yield self.singleAttribute
+						#	self.singleAttribute = ''
+						#else:
+						#	LOGGER.info("LF and  attribute null will be ignore")
+						self.singleAttribute = ''
 				# 处理结束标签，处理跟结束符‘/’没有使用空格分隔的属性“rel=alternate/>”
 				elif char == '/' and not self.stopCode and self.scopeLable != ">":
 					LOGGER.debug("return attribute content is {0}.end char '/'".format(self.singleAttribute))
-					if self.singleAttribute:
-						diff = yield self.singleAttribute
-						self.singleAttribute = ''
+					if self.singleAttribute != '<':
+						if self.singleAttribute:
+							diff = yield self.singleAttribute
+							self.singleAttribute = ''
+					self.nonspace = False
 				self.singleAttribute += char
 				# print(self.singleAttribute)
 				# 拆分结束标签跟结束符">"
 				if not self.stopCode:
-					if self.endMarkParttern.match(self.singleAttribute):
+					atbendptn = self.endMarkParttern.match(self.singleAttribute)
+					if atbendptn:
 						LOGGER.debug("return stop lable attribute is {0}.".format(self.singleAttribute))
-						diff = yield self.singleAttribute
-						self.scopeLable = '>'
-						self.singleAttribute = ''
+
+						if atbendptn.group(1) in self.s_LableList:
+							self.scriptlablestatus = "readly"
+							if self.persingleAttribute.strip():
+								diff = yield self.persingleAttribute
+							diff = yield self.singleAttribute
+							self.scopeLable = '>'
+							self.singleAttribute = ''
+							self.persingleAttribute = ''
+							self.nonspace = True
+						else:
+							self.scopeLable = '>'
 					# 不标准结束符做拆分
 					elif char == ">":
 						if self.scopeLable == "<":
 							self.scopeLable = '>'
 							
-							t_singleAttribute = self.singleAttribute[:-1]
-							if t_singleAttribute:
+							t_singleAttribute = self.singleAttribute[:-1].strip()
+							if t_singleAttribute == "/"  and self.scriptlablestatus != "handling":
 								LOGGER.debug("return before stop lable attribute is {0}.".format(self.singleAttribute))
-								diff = yield t_singleAttribute
-							LOGGER.debug("return stop lable attribute is {0}.".format(self.singleAttribute))
-							diff = yield char
+								self.scriptlablestatus = "readly"
+								diff = yield self.singleAttribute
+								self.nonspace = True
+								LOGGER.debug("return stop lable attribute is {0}.".format(self.singleAttribute))
+							elif self.s_LableParttern.match(t_singleAttribute) or self.scriptlablestatus != "handling":
+								if t_singleAttribute:
+									diff = yield t_singleAttribute
+
+								diff = yield char
+								self.scriptlablestatus = "handling"
+							self.singleAttribute = ""
 						else:
-							LOGGER.debug("return style error attribute is {0}.".format(self.singleAttribute))
-							diff = yield self.singleAttribute
-							LOGGER.warn("date style error!date:{0}.".format(line))
-						self.singleAttribute = ''
+							LOGGER.info("return style error attribute is {0}.".format(self.singleAttribute))
+							#diff = yield self.singleAttribute
+							LOGGER.info("date style error!date:{0}.".format(line))
+						#self.singleAttribute = ''
 	def pagesplit(self,anayurl,dn_queue):
 		"""
 		anayurl:/webpage/website/*
@@ -388,8 +467,11 @@ class splitContent():
 			return json_str,self.lableCount
 
 if __name__ == '__main__':
-	line = 'webpage/blog.csdn.net/blog.csdn.net/sicofield/article/details/8635351'
-	line2 = 'C:\\Users\\belief\\Desktop\\Mr.S\\GitHub\\Infinity_crawler\\webpage\\blog.csdn.net\\sicofield\\article\\details\\8635351'
-	splitcontent = splitContent()
-	itercontent = splitcontent.pagesplit(['http',line2])
+	line = 'webpage/blog.csdn.net/sicofield/article/details/8635351'
+	line2 = 'C:/Users/belief/Desktop/Mr.S/GitHub/Infinity_crawler/webpage/blog.csdn.net#'
+	line3 = 'C:/Users/belief/Desktop/Mr.S/GitHub/Infinity_crawler/webpage/aa'
+	queue = Queue(1000)
+	splitcontent = splitContent(lambda x:print("a"))
+	itercontent,lablesnumber = splitcontent.pagesplit(['http',line2],queue)
+	print(repr(lablesnumber)+":"+itercontent)
 
